5a59df4056ce1e044365d689a11913ef1fecb4f0
Eldad Zipori <eldadzz@student.ubc.ca>
2024-10-15 15:27:54 -0700

    intermidiate

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,132 @@
 #include <Arduino.h>
 
-#define AZI_MOTOR       PA8    // Note that the pin must be PWM compatible
-#define AZI_MOTOR_PWM   PA_8    // has to be in this weird underscore format
-#define AZI_MOTOR_DIR   PB13 
+// Pin definitions
+#define AZI_MOTOR       PA8    // PWM compatible pin for motor control
+#define AZI_MOTOR_PWM   PA_8   // Motor PWM control
+#define AZI_MOTOR_DIR   PB13   // Motor direction control
 
-#define ENC_AZI_A           PB9
-#define ENC_AZI_B           PB8
+#define ENC_AZI_A       PB9    // Encoder Channel A
+#define ENC_AZI_B       PB8    // Encoder Channel B
 
-#define LIMIT_AZI_BTN       PB12
+#define LIMIT_AZI_BTN   PB12   // Calibration switch
+#define STATUS_LED      PC13   // Status LED
 
-#define STATUS_LED          PC13
+// Variables
+volatile long encoderPosition = 0;  // Current encoder position
+const int encoderResolution = 600;  // Pulses per revolution
+bool direction = true;  // Motor direction: true = CW, false = CCW
+int targetAngle = 0;  // Target angle to move to after calibration
 
-bool azi_motor_dir = HIGH;
 int buttonState = 0;
 
+// Function to read encoder
+void readEncoder() {
+  static bool lastA = LOW;
+  bool A = digitalRead(ENC_AZI_A);
+  bool B = digitalRead(ENC_AZI_B);
+  
+  // Check for rising edge on channel A
+  if (A != lastA && A == HIGH) {
+    if (B == LOW) {
+      encoderPosition++;
+      direction = true;  // CW
+    } else {
+      encoderPosition--;
+      direction = false; // CCW
+    }
+  }
+  lastA = A;
+}
 
-volatile unsigned int temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
 
-void ai0() {
-  // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
-  // Check pin 3 to determine the direction
-  if(digitalRead(ENC_AZI_B)==LOW) {
-    counter++;
-  }else{
-    counter--;
+void moveMotor(bool dir, int pwmValue) {
+  digitalWrite(AZI_MOTOR_DIR, dir);  // Set direction
+ ///analogWrite(AZI_MOTOR_PWM, pwmValue);  // Set motor speed
+  pwm_start(AZI_MOTOR_PWM, 500, 50,
+        	TimerCompareFormat_t::PERCENT_COMPARE_FORMAT);
+}
+
+void stopMotor() {
+  pwm_start(AZI_MOTOR_PWM, 500, 0,
+        	TimerCompareFormat_t::PERCENT_COMPARE_FORMAT);
+}
+
+void moveToCalibrationSwitch() {
+  // Rotate motor until calibration switch is hit
+  while (digitalRead(LIMIT_AZI_BTN) == LOW) {
+    moveMotor(true, 50);  // Move in CW direction
   }
+  stopMotor();
+  encoderPosition = 0;  // Reset encoder position
+  Serial.println("Calibration complete, encoder reset.");
+}
+
+void moveToAngle(int angle) {
+  // Calculate target position based on angle
+  long targetPosition = (long)encoderResolution * angle / 360;
+
+  // Move motor until target position is reached
+  while (encoderPosition != targetPosition) {
+    if (encoderPosition < targetPosition) {
+      moveMotor(true, 150);  // Move CW
+    } else {
+      moveMotor(false, 150);  // Move CCW
+    }
   }
-   
-  void ai1() {
-  // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
-  // Check with pin 2 to determine the direction
-  if(digitalRead(ENC_AZI_A)==LOW) {
-    counter--;
-  }else{
-    counter++;
-  }
-  }
+  stopMotor();
+  Serial.print("Reached angle: ");
+  Serial.println(angle);
+}
 
 void setup() {
-  Serial.begin (9600);
-
-  pinMode(LIMIT_AZI_BTN, INPUT_PULLDOWN);
-  pinMode(STATUS_LED, OUTPUT);
-
-
-  // put your setup code here, to run once:
+  // Motor control pins
   pinMode(AZI_MOTOR, OUTPUT);
   pinMode(AZI_MOTOR_DIR, OUTPUT);
-
-  pinMode(ENC_AZI_A, INPUT_PULLUP); // internal pullup input pin 2 
   
-  pinMode(ENC_AZI_B, INPUT_PULLUP); // internalเป็น pullup input pin 3
-  //Setting up interrupt
-  //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
-  attachInterrupt(digitalPinToInterrupt(ENC_AZI_A), ai0, RISING);
-   
-  //B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
-  attachInterrupt(digitalPinToInterrupt(ENC_AZI_B), ai1, RISING);
+  // Encoder pins
+  //pinMode(ENC_AZI_A, INPUT);
+  //pinMode(ENC_AZI_B, INPUT);
   
+  // Calibration switch
+  pinMode(LIMIT_AZI_BTN, INPUT_PULLDOWN);
+  
+  // Status LED
+  pinMode(STATUS_LED, OUTPUT);
+  
+  // Attach interrupt for encoder readings
+  //attachInterrupt(digitalPinToInterrupt(ENC_AZI_A), readEncoder, CHANGE);
+  
+  // Serial for debugging
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  pwm_start(AZI_MOTOR_PWM, 20000, 20,
-        	TimerCompareFormat_t::PERCENT_COMPARE_FORMAT);
+  // Example flow:
+  
+  // Step 1: Move to calibration switch
+  moveToCalibrationSwitch();
+  
+  
+  // Step 2: Set target angle (can be provided via Serial or predefined)
+  //targetAngle = 90;  // Example angle: 90 degrees
+  //buttonState = digitalRead(LIMIT_AZI_BTN);
+  
 
-  if( counter != temp ){
-    Serial.println (counter);
-    temp = counter;
-  }
-
-    // Read the state of the button
-  buttonState = digitalRead(LIMIT_AZI_BTN);
-
-  // If the button is pressed (LOW, because of pull-up resistor)
-  if (buttonState == HIGH) {
-    // Turn on the LED
+  // Button is working
+  /*if (buttonState == HIGH) {
     digitalWrite(STATUS_LED, HIGH);
+
   } else {
-    // Turn off the LED
     digitalWrite(STATUS_LED, LOW);
-  }
+  }*/
 
-  // Small delay for debouncing (can be adjusted if needed)
-  //delay(50);
+  // Step 3: Move to target angle
+  //moveToAngle(targetAngle);
+
+  // Turn on status LED to indicate task completion
+ // digitalWrite(STATUS_LED, HIGH);
+  
+  // Prevent further action in loop
+  //while (true);
+  while(true);
 }
-
